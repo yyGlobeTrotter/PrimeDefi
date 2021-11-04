e33f9aae694dbe3747dc135827eb105f56704ef1
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-05 02:46:41 +0700

    adding more pages

---
+++ client/src/pages/dashboard/investor/Bidding.tsx
@@ -0,0 +1,8 @@
+import { RouteComponentProps } from "@reach/router";
+
+// eslint-disable-next-line
+const Bidding = (_props: RouteComponentProps): JSX.Element => {
+	return <></>;
+};
+
+export default Bidding;
---
+++ client/src/pages/dashboard/issuer/CreateDeal.tsx
@@ -0,0 +1,8 @@
+import { RouteComponentProps } from "@reach/router";
+
+// eslint-disable-next-line
+const CreateDeal = (_props: RouteComponentProps): JSX.Element => {
+	return <></>;
+};
+
+export default CreateDeal;
