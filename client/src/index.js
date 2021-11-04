f4bfc3da946ab000b48b2f611fd62151ca0c8e81
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-04 19:28:56 +0700

    add prettier + eslint & wrapping chakraProvider + MoralisProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ChakraProvider } from "@chakra-ui/react";
+import { MoralisProvider } from "react-moralis";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   <StrictMode>
+      <ChakraProvider>
+         <MoralisProvider appId="xxxxxxxx" serverUrl="xxxxxxxx">
+            <App />
+         </MoralisProvider>
+      </ChakraProvider>
+   </StrictMode>,
+   document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
