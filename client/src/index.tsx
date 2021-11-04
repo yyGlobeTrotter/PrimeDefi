30d29cd0710adb194309a7ed8a0f596ce30166bc
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-04 23:25:29 +0700

    adding eslint and prettier

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,20 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { MoralisProvider } from 'react-moralis';
-import { ChakraProvider } from "@chakra-ui/react"
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { MoralisProvider } from "react-moralis";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
 ReactDOM.render(
-  <StrictMode>
-    <ChakraProvider>
-      <MoralisProvider
-        appId={process.env.REACT_APP_MORALIS_APP_ID ?? ""}
-        serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL ?? ""}
-      >
-        <App />
-      </MoralisProvider>
-    </ChakraProvider>
-  </StrictMode>,
-  document.getElementById('root')
+	<StrictMode>
+		<ChakraProvider>
+			<MoralisProvider
+				appId={process.env.REACT_APP_MORALIS_APP_ID ?? ""}
+				serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL ?? ""}
+			>
+				<App />
+			</MoralisProvider>
+		</ChakraProvider>
+	</StrictMode>,
+	document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
