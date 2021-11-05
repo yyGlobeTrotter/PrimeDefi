026e155c48f2328242211d3fba4cffcdf456843c
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-05 12:00:40 +0700

    importing some routes for pages

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import { Router, RouteComponentProps } from "@reach/router";
 import Dashboard from "./Dashboard";
 import Wallet from "./Wallet";
+import CreateDeal from "./issuer/CreateDeal";
+import Bidding from "./investor/Bidding";
+import Holdings from "./investor/Holdings";
 
 // eslint-disable-next-line
 const Index = (_props: RouteComponentProps): JSX.Element => {
@@ -8,6 +11,9 @@ const Index = (_props: RouteComponentProps): JSX.Element => {
 		<Router>
 			<Dashboard path="dashboard" />
 			<Wallet path="wallet" />
+			<CreateDeal path="create-deal" />
+			<Bidding path="bidding" />
+			<Holdings path="holdings" />
 		</Router>
 	);
 };
