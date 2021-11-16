c8aab80b36c38e3a1fad028d6afae0358873edb3
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2021-11-16 21:34:02 +0700

    added createDeal name to input

--- client/src/list/createDeal.ts
+++ client/src/list/createDeal.ts
@@ -1,5 +1,6 @@
 export interface CreateDealTextFieldsType {
 	title: string;
+	name: string;
 	disabled?: boolean;
 	subtitle?: string;
 	placeholder?: string;
@@ -14,33 +15,39 @@ export interface CreateDealTextFieldsType {
 const createDealTextFields: Array<CreateDealTextFieldsType> = [
 	{
 		title: "ISIN Number",
+		name: "ISINNumber",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Deal Name",
+		name: "dealName",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Initial Offer Size",
+		name: "initialOfferSize",
 		type: "number",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Min. Launch Size",
+		name: "minLaunchSize",
 		type: "number",
 		xs: 12,
 		md: 6,
 	},
 	{
-		title: "Face Value (Per Unit",
+		title: "Face Value (Per Unit)",
+		name: "faceValue",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Amount Required for Escrow",
+		name: "amountRequiredForEscrow",
 		disabled: true,
 		subtitle: "This is auto-calculated based on the 5% initial offer size",
 		xs: 12,
@@ -48,28 +55,33 @@ const createDealTextFields: Array<CreateDealTextFieldsType> = [
 	},
 	{
 		title: "Offer Date Range",
+		name: "offerDateRange",
 		placeholder: "DD/MM/YYYY - DD/MM/YYYY",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Term (in days)",
+		name: "term",
 		xs: 12,
 		md: 3,
 	},
 	{
 		title: "Interest Rate (in %)",
+		name: "interestRate",
 		xs: 12,
 		md: 3,
 	},
 	{
 		title: "Bond Issue Date (optional)",
+		name: "bondIssueDate",
 		placeholder: "DD/MM/YYYY",
 		xs: 12,
 		md: 6,
 	},
 	{
 		title: "Upfront fee (in %)",
+		name: "upfrontFee",
 		xs: 12,
 		md: 3,
 	},
