64c8ae244932803797a6cc65a7a3a073198b70f0
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2021-11-17 09:51:35 +0700

    add enableWeb3 + adding package.json for truffle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { Router } from "@reach/router";
+import { useMoralis } from "react-moralis";
 import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard";
 
 const App = (): JSX.Element => {
+	const { isAuthenticated, isWeb3Enabled, isWeb3EnableLoading, enableWeb3 } =
+		useMoralis();
+
+	useEffect(() => {
+		if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) {
+			enableWeb3();
+		}
+		// eslint-disable-next-line
+	}, [isAuthenticated, isWeb3Enabled]);
+
 	return (
 		<Router>
 			<Login path="login" />
