40cb9db222bdc46f2f1bedf56867258b7a826038
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2021-11-21 21:49:26 +0700

    add truffle config + migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Deal = artifacts.require("Deal");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Deal);
 };
